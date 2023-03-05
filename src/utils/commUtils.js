9c82aece1060e335af21081b667cca982d8709b6
dejunqi <dejunqi2008@gmail.com>
2023-03-04 16:46:15 -0800

    mend

--- src/utils/commUtils.js
+++ src/utils/commUtils.js
@@ -3,6 +3,7 @@ import moment from "moment/moment";
 
 const getBaseAPIUrl = () => {
     console.log('getBaseAPIUrl', process.env.NODE_ENV )
+    console.log('CUSTOM_NODE_ENV', process.env.CUSTOM_NODE_ENV);
     const isDevEnv = process.env.NODE_ENV === 'development';
     return  isDevEnv ? 'http://localhost:8000/api': 'https://dejun-blog-server.herokuapp.com/api';
 }

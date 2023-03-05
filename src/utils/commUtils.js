5af5f8bbc1da49c5248167449a732a8cf02ccb82
dejunqi <dejunqi2008@gmail.com>
2023-03-04 19:04:25 -0800

    mend

--- src/utils/commUtils.js
+++ src/utils/commUtils.js
@@ -2,19 +2,8 @@ import moment from "moment/moment";
 
 
 const getBaseAPIUrl = () => {
-    // console.log('getBaseAPIUrl', process.env.NODE_ENV )
-    // console.log('BASE_API_URL', process.env.BASE_API_URL);
-    // const isDevEnv = process.env.NODE_ENV === 'development';
-    // return  isDevEnv ? 'http://localhost:8000/api': 'https://dejun-blog-server.herokuapp.com/api';
-
-    if (process.env) {
-        console.log(process.env);
-        console.log(process.env.REACT_APP_BASE_URL);
-    } else {
-        console.log(process);
-    }
-
-    return 'https://dejun-blog-server.herokuapp.com/api';
+    return !!process.env.REACT_APP_BASE_URL ? 
+        process.env.REACT_APP_BASE_URL : 'http://localhost:8000/api';
 }
 
 export const baseAPIUrl = getBaseAPIUrl();

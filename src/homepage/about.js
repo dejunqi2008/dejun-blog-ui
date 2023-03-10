588a825c1f23b535ed9808bed4dac7524b92cce7
dejunqi <dejunqi2008@gmail.com>
2023-03-10 14:55:50 -0800

    fix image display issue with temp solution

--- src/homepage/about.js
+++ src/homepage/about.js
@@ -60,9 +60,9 @@ export const UserIntro = () => {
     }
 
     const getProfilePic = () => {
-        let addr = profilepic || 'https://raw.githubusercontent.com/dejunqi2008/dejun-blog-ui/mainline/public/default-profile.jpeg';
+        let addr = profilepic || "https://uc7c8d076c46caabb5adfe624e24.previews.dropboxusercontent.com/p/thumb/AB1bdO2KGZBJ15VSp1gKJqX5Dmv4w3rDZKoxopo0LTgLLecgJc221Dm9IbuYiqf4fpym7SK0RTLBr8Wp13I-U2q-MR5Szkm_EAvHUkbkPt_bbi_dfQsipUyKAk0Y3ywNS1pG3kB-u9ZhJhzzzM1Dy_FCv-8keSWg8gKeLN9kdOHHy2K5jG3borIpiaa2sQX_NeoDavjCv0Y6WUtZKEalXEf3moCv2BlWGWHShqJWQLDO3aM4dsjUPfHrQQwRfBs9858wxv2j0lMJTM3-lrFKXEPPKTyQJqYcyXBRuN4FmbYB-yLkDXO6LjbVAC74-AHAsRYFaThlleMJfaebrnzhvGwzRcJYvr_wDYRohl3zXbP61_awRLFR_o2pmH0nE9hOqjq8n20fX0mEFV3xxFXkx_ZYHCfPUDMom0rlm34-MleSVg/p.png";
         if (isadmin) {
-            addr = 'https://raw.githubusercontent.com/dejunqi2008/dejun-blog-ui/mainline/public/profile.jpeg';
+            addr = "https://uca91500848a944713fb81fcd5b8.previews.dropboxusercontent.com/p/thumb/AB000uHwxlp8rK3aqf636pEytGpDsbJUyWNVHHQm7iocFXsYr6eVcyLhyx1eUXDgPOq3OrP08N0xTllbtgCcwA5dOfwx-MU5BsgX8Bp5M0Y5VeOpGRRz953cmhctw8P6U6ogU0AVpH_CpFPFXWvvJRIW4xLRcfYm_jZ_WfNqLD3ae-u1GTj_dfAWXnMDIxeWwLtu82VD3uIdeE-NyYGZ2m-p82SqDKGI0-Dlcq3szqnl2hQGsAyiNxdM9b6168Q4AikrPGq6XeivD4oVJYjsIiljAyQdCl824lGi6wuS6Nccg3M6yezvDDAjJfIgljeCAUtN7RNBlfbGgYmX5BbyRTk4rOToNWFJAWV58WkNZKU0h9jY3A-AQr468HZQdSOYRm8/p.jpeg?size=512x512&size_mode=1";
         }
         return addr;
     }
